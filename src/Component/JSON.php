c58d4f5fb5c4d51445791d55c6cbb9cda0b086b2
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-01-09 12:33:45 +0330

    Improve PHPDoc comments
    And a hash change in composer.

--- src/Component/JSON.php
+++ src/Component/JSON.php
@@ -1,9 +1,9 @@
 <?php
-
 /**
- * @author Mohammad Amin Chitgarha <machitgarha@outlook.com>
- * @see https://github.com/MAChitgarha
+ * The summary of the file
+ * 
  */
+
 namespace MAChitgarha\Component;
 
 /**
@@ -83,7 +83,7 @@ class JSON
     /**
      * Returns data as JSON.
      *
-     * @param int $options The options, like JSON_PRETTY_PRINT. {@see http://php.net/json.constants}
+     * @param int $options The options, like JSON_PRETTY_PRINT. {@link http://php.net/json.constants}
      * @return string
      */
     public function getDataAsJson(int $options = 0)
@@ -231,7 +231,7 @@ class JSON
         if (count($keys) === 0) {
             $this->setKey($currentKey, $data, $value);
             return $this;
-        // Recurse on remained keys
+        // Recurs on remained keys
         } else {
             /*
              * If the current key does not exist, set it to an empty array or an object based on
@@ -268,7 +268,7 @@ class JSON
         if ($index === "")
             return [""];
 
-        // Explode index parts by $delmiter
+        // Explode index parts by $delimiter
         return explode($delimiter, $index);
     }
 
@@ -302,7 +302,7 @@ class JSON
      * Determines if an index exists or not.
      *
      * @param string $index The index. It can contain several nested keys separated by dots (like JavaScript).
-     * @return boolean
+     * @return boolean Whether the index is set or not. A null value will be considered as not set.
      */
     public function isSet(string $index)
     {
