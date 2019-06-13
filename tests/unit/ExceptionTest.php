9bce212c385aef884b02a035f14a6e5dc9e9ee8c
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-06-14 02:00:38 +0430

    Add ExceptionTest.php for testing \Exception

---
+++ tests/unit/ExceptionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Unit tests for MAChitgarha\Component\JSON class.
+ *
+ * Go to the project's root and run the tests in this way:
+ * phpunit --bootstrap vendor/autoload.php tests/unit
+ *
+ * @see MAChitgarha\Component\JSON
+ */
+namespace MAChitgarha\UnitTest\JSON;
+
+use PHPUnit\Framework\TestCase;
+use MAChitgarha\Component\JSON;
+
+/**
+ * Expect \Exception in all of these tests.
+ */
+class ExceptionTest extends TestCase
+{
+    /** @var JSON */
+    protected $json;
+
+    protected function setUp()
+    {
+        $this->expectException(\Exception::class);
+
+        // Set up fixtures
+        $this->json = new JSON();
+    }
+
+    /**
+     * Tests invalid JSON strings.
+     *
+     * @dataProvider invalidJsonProvider
+     */
+    public function testInvalidJsonStrings(string $data)
+    {
+        new JSON($data, JSON::OPT_TREAT_AS_JSON_STRING);
+    }
+
+    /** @todo Import JSON schema. */
+    public function invalidJsonProvider()
+    {
+        return [
+            ["[] // Commenting"],
+            ["{'user_id':1234}"],
+            ["[function () {}]"],
+            ["{color: \"red\"}"],
+            ["[0,1,2,3,4,5,6,]"],
+        ];
+    }
+}
