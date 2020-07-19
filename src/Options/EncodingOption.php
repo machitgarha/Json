608c3ed30117508c63e7612fa1c1904a599c1b6b
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-07-19 21:57:08 +0430

    Add EncodingOption class for provding options for Json::encode()
    Add a new option also: PRETTY_PRINT.

---
+++ src/Options/EncodingOption.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MAChitgarha\Json\Options;
+
+/**
+ * Option provider for Json::encode().
+ *
+ * @see Json::encode()
+ */
+class EncodingOption
+{
+    /**
+     * Make the output more human-readable by indentions and whitespaces.
+     * @var int
+     */
+    const PRETTY_PRINT = 1;
+}
