b26528156b88134f3cc09c205e0a389ccd32370d
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-07-24 21:13:38 +0430

    Add data/interactor-auto-detection.php for auto-detection feature
    Read the file comments for more details.

---
+++ data/interactor-auto-detection.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Auto-detection data for supported classes with existing interactors.
+ *
+ * Because array indexing is much more efficient and performant than other methods (e.g.
+ * searching all the array for a specific value), and also because the main handler of
+ * auto-detection is the provider class name (along with its namespace), the first depth
+ * of the array contains key-value pairs, in which, the keys are the provider name, and
+ * the values is an array of mapping functionalities into prefered interactors.
+ *
+ * "Talk is cheap, show me the code" (TM) (Linus Torvalds). See the array structure to
+ * understand the description above.
+ *
+ * @var array
+ */
+return [
+];
